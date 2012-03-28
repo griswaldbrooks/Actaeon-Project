a01bd45277d404b4316fcb0e94389b8893b13ff4
Griswald Brooks <ricecracker02@gmail.com>
2012-03-27 23:39:05 -0400

    Tuning left wheel PID

--- PIDCtrlr/picoBoard.c
+++ PIDCtrlr/picoBoard.c
@@ -24,10 +24,10 @@
 #define LEFT_WHEEL_TIMEOUT		0.15	// sec
 #define M_2PIR					31.4159	// Constant for convering from cycles per second to cm/sec
 
-void pwm_setup(void){
+#define wheel_l_on()	PWM_timer1_On_Pin9();
+#define wheel_r_on()	PWM_timer1_On_Pin10();
 
-	PWM_timer1_On_Pin9();
-	PWM_timer1_On_Pin10();
+void pwm_setup(void){
 
 	// 20 ms period
 	timer1PWMInitICR(5000);
@@ -77,6 +77,9 @@ void setup_hardware(void){
 
 void wheel_r(float cmd_vel_r){
 	u16 duty;
+	// Wheel deadband 8cm/sec > v_dead > -5cm/sec
+	if((cmd_vel_r < 8) && (cmd_vel_r > -8))  cmd_vel_r = 0;
+
 	// Limit velocity commands, cm/sec
 	if(cmd_vel_r > 32)  cmd_vel_r =  32;
 	if(cmd_vel_r < -32) cmd_vel_r = -32;
@@ -88,6 +91,9 @@ void wheel_r(float cmd_vel_r){
 
 void wheel_l(float cmd_vel_l){
 	u16 duty;
+	// Wheel deadband 7cm/sec > v_dead > -9cm/sec
+	if((cmd_vel_l < 9) && (cmd_vel_l > -9))  cmd_vel_l =  0;
+
 	// Limit velocity commands, cm/sec
 	if(cmd_vel_l > 32)  cmd_vel_l =  32;
 	if(cmd_vel_l < -32) cmd_vel_l = -32;
@@ -112,7 +118,8 @@ int main(void){
 
 	u08 key;
 	u08 counter = 1;
-
+	
+	// Filter values for velocity encoding
 	float rps_r = 0;
 	float rps_r_prev = 0;
 	float rps_rf = 0;
@@ -126,18 +133,61 @@ int main(void){
 	float rps_lf_prev = 0;
 	float Kl = 10;
 	float dt_l = 0;
-
-	float Kp_l = 1;
-	float error_l;
+	////////////////////////////////////////
+	
+	
 
 	float v_l;
 	float v_r;
+	float v_l_cmd = 18;
+	float v_r_cmd;
+
+	float v_l_out;
+	
+	// PID constants //////////////////////////////////////////////////////////////
+
+	float error_l_n1, error_l;							// Error, previous
+	float u_l, up_l, ui_l, ud_l;						// Output, PID
+	float ui_l_n1;										// Previous integral
+	float ud_l_n1, udf_l, udf_l_n1;						// Previous derivative, filtered derivative value and previous filtered derivative value
+	float Kp_l, Ki_l, Kd_l, K_l, T_l, Ti_l, Td_l;		// PID Constants
+	float lpf1_l, lpf2_l, lpf3_l;								// Low pass filter constants
+	float wb = 1256.6;									// Break frequency in radians/sec
+	
+	// Initialize memory
+	error_l_n1 = error_l = u_l = 0;
+	ud_l_n1 = udf_l = udf_l_n1 = 0;
+	
+	//Ku = 0.002
+	//K_l = 0.0012;
+	K_l = 0.025;									// Tuned value
+	Ti_l = 1.1945;
+	Td_l = 0.2986;
+	T_l = 2.3890;										// sec
+		
+	Kp_l = K_l;											// Proportional constant
+	Ki_l = (K_l*T_l)/(2*Ti_l);							// Integral constant
+	Kd_l = (2*K_l*Td_l)/T_l;							// Derivative constant
+	
+	lpf1_l = (wb*T_l)/(2 + wb*T_l);
+	lpf2_l = lpf1_l;
+	lpf3_l = (wb*T_l - 2)/(wb*T_l + 2);
+	///////////////////////////////////////////////////////////////////////////////////////
+	
 	
 	setup_hardware();
-	//PWM_timer1_Set_Pin9(0);
-	//PWM_timer1_Set_Pin10(0);
+	
 	
 /**/
+	wheel_l_on();
+	wheel_r_on();
+	wheel_l(0);
+	wheel_r(0);
+	
+	for(u16 ndx = 0; ndx < 600; ndx++){
+		delay_us(200);
+	}
+
 	while(1){
 		elapsed_time_l = elapsed_time_r = ((get_timer0_overflow()*255 + TCNT0) * 0.0435) / 10000;
 		
@@ -200,18 +250,57 @@ int main(void){
 		
 		v_l = M_2PIR*rps_lf;		
 		v_r = M_2PIR*rps_rf;
+		
+		// PID calculation	/////////////////////////////////////////
+		/////////////////////////////////////////////////////////////
+		/////////////////////////////////////////////////////////////
 
-		wheel_l(12);
+		error_l = v_l_cmd - v_l;									// Current Left Wheel error
+	
+		// Calculate the proportion
+		up_l = Kp_l*error_l;										// Left Wheel Proportion
+		
+		// Calculate the integral
+		ui_l = Ki_l*(error_l + error_l_n1) + ui_l_n1;				// Update Left Wheel integral
 
-		wheel_r(12);
+		// Calculate the derivative		
+		ud_l = Kd_l*(error_l - error_l_n1) - ud_l_n1;				// Update Left Wheel derivative
+		
+		// Filter the derivative output
+		udf_l = lpf1_l*ud_l + lpf2_l*ud_l_n1 - lpf3_l*udf_l_n1;		// Update filtered Left Wheel derivative
+		
+		u_l = up_l + ui_l + udf_l;									// u_l(t) to be output to the Left Wheel 
+		
+		// Anti-windup
+		if(u_l > 30){
+			ui_l = ui_l_n1;
+			u_l = 30;	
+		}
+		else if(u_l < -30){
+			ui_l = ui_l_n1;
+			u_l = -30;
+		}
+
+		error_l_n1 = error_l;						// Update previous Left Wheel proportional error
+		ui_l_n1 = ui_l;								// Update previous Left Wheel integral
+		ud_l_n1 = ud_l;								// Update previous Left Wheel derivative
+		udf_l_n1 = udf_l;							// Update previous Left Wheel filtered derivative
+
+		/////////////////////////////////////////////////////////////
+		/////////////////////////////////////////////////////////////
+		/////////////////////////////////////////////////////////////
+		
+		
+		wheel_l(u_l);
+		wheel_r(0);
 
 		//rprintf("Left: ");
 		rprintfFloat(5,v_l);
 		//rprintf("Right: ");
-		rprintf("\t,");
-		rprintfFloat(5,v_r);
-		rprintf("\t,");
-		rprintfFloat(5,elapsed_time_l);
+		//rprintf("\t,");
+		//rprintfFloat(5,v_r);
+		//rprintf("\t,");
+		//rprintfFloat(5,elapsed_time_l);
 		rprintfCRLF();
 		
 		
